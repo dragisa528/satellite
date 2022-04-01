061070d97e7985ee8da8775e187cf8a2dc5d2c85
techking5287 <techking5287@gmail.com>
2022-04-01 16:39:24 +0100

    Scaffolding

---
+++ satellite.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Satellite
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     A collection of developer tools for WordPress projects
+ * Author:          Orphans Web Team
+ * Author URI:      https://orphans.co.uk
+ * Text Domain:     satellite
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Satellite
+ */
+
+// Your code starts here.
