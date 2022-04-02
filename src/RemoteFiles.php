3b29c249007b1c9bce405fe8cb9c3494334272a3
techking5287 <techking5287@gmail.com>
2022-04-02 14:12:34 +0100

    Merderise & clean up RemoteFiles code

--- src/RemoteFiles.php
+++ src/RemoteFiles.php
@@ -16,41 +16,17 @@ class RemoteFiles {
 
 	/**
 	 * Production URL
-	 *
-	 * @since 1.0.0
-	 * @var string
 	 */
-	public $production_url = '';
+	public string $production_url = '';
 
 	/**
 	 * Holds list of upload directories
 	 * Can set manually here, or allow function below to automatically create it
-	 *
-	 * @since 1.0.0
-	 * @var array
 	 */
-	public $directories = array();
-
-	/**
-	 * Start Month
-	 *
-	 * @since 1.0.0
-	 * @var int
-	 */
-	public $start_month = false;
-
-	/**
-	 * Start Year
-	 *
-	 * @since 1.0.0
-	 * @var int
-	 */
-	public $start_year = false;
+	public array $directories = array();
 
 	/**
 	 * Primary constructor.
-	 *
-	 * @since 1.0.0
 	 */
 	function __construct() {
 
@@ -61,36 +37,23 @@ class RemoteFiles {
 		add_filter( 'the_content', array( $this, 'image_content' ) );
 		add_filter( 'the_content', array( $this, 'image_content_relative' ) );
 		add_filter( 'wp_get_attachment_url', array( $this, 'update_image_url' ) );
-
 	}
 
 	/**
 	 * Modify Main Image URL
-	 *
-	 * @param array $image
-	 *
-	 * @return array $image
-	 * @since 1.0.0
 	 */
-	function image_src( $image ) {
-
+	function image_src( array $image ): array {
 		if ( isset( $image[0] ) ) {
 			$image[0] = $this->update_image_url( $image[0] );
 		}
 
 		return $image;
-
 	}
 
 	/**
 	 * Modify Image Attributes
-	 *
-	 * @param array $attr
-	 *
-	 * @return array $attr
-	 * @since 1.0.0
 	 */
-	function image_attr( $attr ) {
+	function image_attr( array $attr ): array {
 
 		if ( isset( $attr['srcset'] ) ) {
 			$srcset = explode( ' ', $attr['srcset'] );
@@ -101,21 +64,13 @@ class RemoteFiles {
 		}
 
 		return $attr;
-
 	}
 
 	/**
 	 * Modify Image for Javascript
 	 * Primarily used for media library
-	 *
-	 * @param array $response Array of prepared attachment data
-	 * @param int|object $attachment Attachment ID or object
-	 * @param array $meta Array of attachment metadata
-	 *
-	 * @return array     $response   Modified attachment data
-	 * @since 1.3.0
 	 */
-	function image_js( $response, $attachment, $meta ) {
+	function image_js( array $response ): array {
 
 		if ( isset( $response['url'] ) ) {
 			$response['url'] = $this->update_image_url( $response['url'] );
@@ -130,13 +85,8 @@ class RemoteFiles {
 
 	/**
 	 * Modify Images in Content
-	 *
-	 * @param string $content
-	 *
-	 * @return string $content
-	 * @since 1.2.0
 	 */
-	function image_content( $content ) {
+	function image_content( string $content ): string {
 		$upload_locations = wp_upload_dir();
 
 		$regex = '/https?:\/\/[^\" ]+/i';
@@ -145,7 +95,6 @@ class RemoteFiles {
 		foreach ( $matches[0] as $url ) {
 			if ( false !== strpos( $url, $upload_locations['baseurl'] ) ) {
 				$new_url = $this->update_image_url( $url );
-				// echo $url."<staing>";
 				$content = str_replace( $url, $new_url, $content );
 			}
 		}
@@ -155,26 +104,15 @@ class RemoteFiles {
 
 	/**
 	 * Modify Images in Content
-	 *
-	 * @param string $content
-	 *
-	 * @return string $content
-	 * @since 1.2.0
 	 */
-	function image_content_relative( $content ) {
-		$upload_locations = wp_upload_dir();
-
+	function image_content_relative( string $content ): string {
 		$regex = '/\"\/app\/uploads[^\" ]+/i';
 		preg_match_all( $regex, $content, $matches );
-		//var_dump($matches);
 
 		foreach ( $matches[0] as $url ) {
-			// if( false !== strpos( $url, $upload_locations[ 'baseurl' ] ) ) {
 			$url     = str_replace( "\"", "", $url );
 			$new_url = $this->update_image_url_relative( $url );
-			// echo $new_url;
 			$content = str_replace( $url, $new_url, $content );
-			// }
 		}
 
 		return $content;
@@ -182,40 +120,24 @@ class RemoteFiles {
 
 	/**
 	 * Convert a URL to a local filename
-	 *
-	 * @param string $url
-	 *
-	 * @return string $local_filename
-	 * @since 1.4.0
 	 */
-	function local_filename( $url ) {
+	function local_filename( string $url ): string {
 		$upload_locations = wp_upload_dir();
-		$local_filename   = str_replace( $upload_locations['baseurl'], $upload_locations['basedir'], $url );
 
-		return $local_filename;
+		return str_replace( $upload_locations['baseurl'], $upload_locations['basedir'], $url );
 	}
 
 	/**
 	 * Determine if local image exists
-	 *
-	 * @param string $url
-	 *
-	 * @return boolean
-	 * @since 1.4.0
 	 */
-	function local_image_exists( $url ) {
+	function local_image_exists( string $url ): bool {
 		return file_exists( $this->local_filename( $url ) );
 	}
 
 	/**
 	 * Update Image URL
-	 *
-	 * @param string $image_url
-	 *
-	 * @return string $image_url
-	 * @since 1.0.0
 	 */
-	function update_image_url( $image_url ) {
+	function update_image_url( string $image_url ): string {
 
 		if ( ! $image_url ) {
 			return $image_url;
@@ -230,27 +152,19 @@ class RemoteFiles {
 			return $image_url;
 		}
 
-		$image_url = str_replace( trailingslashit( home_url() ), trailingslashit( $production_url ), $image_url );
-
-		return $image_url;
+		return str_replace( trailingslashit( home_url() ), trailingslashit( $production_url ), $image_url );
 	}
 
 	/**
 	 * Update Image URL
-	 *
-	 * @param string $image_url
-	 *
-	 * @return string $image_url
-	 * @since 1.0.0
 	 */
-	function update_image_url_relative( $image_url ) {
+	function update_image_url_relative( string $image_url ): string {
 
 		if ( ! $image_url ) {
 			return $image_url;
 		}
 
 		if ( $this->local_image_exists( $image_url ) ) {
-			// echo 'have imagel';
 			return $image_url;
 		}
 
@@ -259,9 +173,7 @@ class RemoteFiles {
 			return $image_url;
 		}
 
-		$image_url = $production_url . $image_url;
-
-		return $image_url;
+		return $production_url . $image_url;
 	}
 
 
@@ -269,12 +181,9 @@ class RemoteFiles {
 	 * Return the production URL
 	 *
 	 * First, this method checks if constant `SATELLITE_PRODUCTION_URL`
-	 * exists and non-empty. Than applies a filter `satellite_url`.
-	 *
-	 * @return string
-	 * @since 1.5.0
+	 * exists and non-empty. Then applies a filter `satellite_url`.
 	 */
-	public function get_production_url() {
+	public function get_production_url(): string {
 		$production_url = $this->production_url;
 		if ( defined( 'SATELLITE_PRODUCTION_URL' ) && SATELLITE_PRODUCTION_URL ) {
 			$production_url = SATELLITE_PRODUCTION_URL;
