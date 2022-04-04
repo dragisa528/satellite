5e9de66586404673e4faa48f62e3860c8c5e61cc
techking5287 <techking5287@gmail.com>
2022-04-04 09:44:27 +0100

    Add update URI

--- satellite.php
+++ satellite.php
@@ -5,6 +5,7 @@
  * Description:     A collection of developer tools for WordPress projects
  * Author:          Orphans Web Team
  * Author URI:      https://orphans.co.uk
+ * Update URI:      https://code.orphans.co.uk/packages/wordpress/satellite/
  * Text Domain:     satellite
  * Domain Path:     /languages
  * Version:         0.1.0
