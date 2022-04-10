9aab0638a8df2f9561d03f24903309ce36b801da
techking5287 <techking5287@gmail.com>
2022-04-10 17:40:18 +0100

    Fix inaccuracy in EnvReader’s namespacing

--- src/Traits/EnvReader.php
+++ src/Traits/EnvReader.php
@@ -10,8 +10,8 @@ trait EnvReader
      */
     private function env(string $key)
     {
-        if (class_exists('Env') && method_exists('Env', 'env')) {
-            return Env\env($key);
+        if (class_exists('\Env\Env') && method_exists('\Env\Env', 'get')) {
+            return \Env\Env::get($key);
         } elseif (function_exists('env')) {
             return env($key);
         }
