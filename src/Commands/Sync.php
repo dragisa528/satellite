6e290341eac1ec190d151e238d7ca3a4291be684
techking5287 <techking5287@gmail.com>
2022-04-10 18:01:49 +0100

    DB sync and plugin (de)activations

--- src/Commands/Sync.php
+++ src/Commands/Sync.php
@@ -23,6 +23,10 @@ class Sync
         'ssh_port' => '22',
         'ssh_user' => null,
         'ssh_path' => null,
+        'plugins' => [
+            'activate' => null,
+            'deactivate' => null,
+        ],
     ];
 
     public function run($args, $assoc_args)
@@ -33,6 +37,10 @@ class Sync
 
         if (!$this->hasAllSettings()) {
             WP_CLI::error('You are missing some config settings in your environment. Please refer to the plugin\'s README.md.');
+        }
+
+        if (!$this->hasRemoteAccess()) {
+            WP_CLI::error('Cannot access remote website. Please check your connection settings.');
         }
 
         $this->getOptions($assoc_args);
@@ -94,6 +102,46 @@ class Sync
                 return false;
             }
         }
+
+        // Check for PV
+        $this->settings['has_pv'] = !empty(`which pv`);
+        if (!$this->settings['has_pv']) {
+            WP_CLI::warning("You may wish to install 'pv' to see progress when running this command.");
+        }
+
+        // Plugin (de)activations
+        $activated_plugins = $deactivated_plugins = null;
+
+        if ($this->env('SATELLITE_SYNC_ACTIVATE_PLUGINS')) {
+            $activated_plugins = $this->env('SATELLITE_SYNC_ACTIVATE_PLUGINS');
+        } else {
+            try {
+                $activated_plugins = Config::get('SATELLITE_SYNC_ACTIVATE_PLUGINS');
+            } catch (UndefinedConfigKeyException $e) {
+                //
+            }
+        }
+        if ($activated_plugins !== null) {
+            $activated_plugins = preg_split('/[\s,]+/', $activated_plugins);
+            $activated_plugins = array_filter($activated_plugins);
+        }
+        $this->settings['plugins']['activate'] = $activated_plugins;
+
+        if ($this->env('SATELLITE_SYNC_DEACTIVATE_PLUGINS')) {
+            $deactivated_plugins = $this->env('SATELLITE_SYNC_DEACTIVATE_PLUGINS');
+        } else {
+            try {
+                $deactivated_plugins = Config::get('SATELLITE_SYNC_DEACTIVATE_PLUGINS');
+            } catch (UndefinedConfigKeyException $e) {
+                //
+            }
+        }
+        if ($deactivated_plugins !== null) {
+            $deactivated_plugins = preg_split('/[\s,]+/', $deactivated_plugins);
+            $deactivated_plugins = array_filter($deactivated_plugins);
+        }
+        $this->settings['plugins']['deactivate'] = $deactivated_plugins;
+
         return true;
     }
 
@@ -108,6 +156,27 @@ class Sync
         }
     }
 
+    private function hasRemoteAccess(): bool
+    {
+        $this->settings['ssh_command'] = "ssh -q -p {$this->settings['ssh_port']} {$this->settings['ssh_user']}@{$this->settings['ssh_host']}";
+
+        # Try SSH
+        $command = "{$this->settings['ssh_command']} exit; echo $?";
+        $live_server_status = exec($command);
+        if ($live_server_status === '255') {
+            WP_CLI::error("Cannot connect to {$this->settings['ssh_user']}@{$this->settings['ssh_host']} over SSH");
+        }
+
+        # Try remote WP-CLI
+        $command = "{$this->settings['ssh_command']} \"bash -c \\\"test -f {$this->settings['ssh_path']}/vendor/bin/wp && echo true || echo false\\\"\"";
+        $live_server_status = exec($command);
+        if ($live_server_status !== 'true') {
+            WP_CLI::error("Cannot find WP-CLI at {$this->settings['ssh_user']}@{$this->settings['ssh_host']}");
+        }
+
+        return true;
+    }
+
     private function printActionTitle(string $title)
     {
         WP_CLI::line(WP_CLI::colorize('%b'));
@@ -118,7 +187,11 @@ class Sync
     private function fetchDatabase()
     {
         $this->printActionTitle('Fetching database');
-        WP_CLI::line(WP_CLI::colorize('%y// todo%n'));
+        WP_CLI::line(WP_CLI::colorize('%y// todo: post-sync queries%n'));
+
+        $pipe = $this->settings['has_pv'] ? ' | pv | ' : ' | ';
+        $command = "{$this->settings['ssh_command']} \"bash -c \\\"cd {$this->settings['ssh_path']} && ./vendor/bin/wp db export --quiet --single-transaction - | gzip -cf\\\"\" {$pipe} gunzip -c | ./vendor/bin/wp db import --quiet -";
+        system($command);
     }
 
     private function fetchUploads()
@@ -129,13 +202,53 @@ class Sync
 
     private function activatePlugins()
     {
+        if (empty($this->settings['plugins']['activate'])) {
+            return;
+        }
+
         $this->printActionTitle('Activating Plugins');
-        WP_CLI::line(WP_CLI::colorize('%y// todo%n'));
+
+        foreach ($this->settings['plugins']['activate'] as $plugin) {
+            $command = "wp plugin is-installed {$plugin}";
+            exec($command, $output, $retval);
+            if ($retval === 0) {
+                $command = "wp plugin is-active {$plugin}";
+                exec($command, $output, $retval);
+                if ($retval !== 0) {
+                    $command = "wp plugin activate {$plugin}";
+                    system($command);
+                } else {
+                    WP_CLI::warning("Plugin {$plugin} is already active");
+                }
+            } else {
+                WP_CLI::warning("Plugin {$plugin} is not available to activate");
+            }
+        }
     }
 
     private function deactivatePlugins()
     {
+        if (empty($this->settings['plugins']['deactivate'])) {
+            return;
+        }
+
         $this->printActionTitle('Deactivating Plugins');
-        WP_CLI::line(WP_CLI::colorize('%y// todo%n'));
+
+        foreach ($this->settings['plugins']['deactivate'] as $plugin) {
+            $command = "wp plugin is-installed {$plugin}";
+            exec($command, $output, $retval);
+            if ($retval === 0) {
+                $command = "wp plugin is-active {$plugin}";
+                exec($command, $output, $retval);
+                if ($retval === 0) {
+                    $command = "wp plugin deactivate {$plugin}";
+                    system($command);
+                } else {
+                    WP_CLI::warning("Plugin {$plugin} is already inactive");
+                }
+            } else {
+                WP_CLI::warning("Plugin {$plugin} is not available to deactivate");
+            }
+        }
     }
 }
